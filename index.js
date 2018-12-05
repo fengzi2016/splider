ad9511450f89b9cc5f9699e2afac70a679a0721c
fengzi2016 <1820166258@qq.com>
2018-12-05 15:22:17 +0800

    feat:add more

--- index.js
+++ index.js
@@ -1,39 +1,66 @@
 var cheerio = require('cheerio');
-var http = require('http');
-var iconv = require('iconv-lite');
-var querystring = require('querystring');
 var request = require('superagent');
+var fs = require('fs');
 
-var requestUrl = 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC100001/';
-request
-    .get(requestUrl)// 转码-将gb2312格式转成utf-8
-    .end(function (err, res) {
-    // 常规的错误处理
-    if (err) {
-      return next(err);
-    }
-    var $ = cheerio.load(res.text,{
-      xmlMode: true // 由于从rss里读取xml，所以这一步一定要有，切记
-    });
-   
-    var d = new Date();
-    var date = d.getFullYear()+"-"+(d.getMonth()+1)+"-"+(d.getDate()); // 取得爬取的日期
-    var topten = { // 设定爬取的json数组
-        date: date,
-        info: []
-      };
-    console.log($('#__p2').text()); // 摘要
-    var json = JSON.stringify(toptens); // json格式解析，这步也是一定要有
+// 组合函数
+const compose = (f,g) => {
+  return function(x) {
+      return f(g(x));
+  }
+}
 
+let count = 642;
+const url = 'http://www.lis.ac.cn/CN/volumn/volumn_642.shtml';
+const urlsSeletor = 'a[^href="../abstract/"]';
+
+// 发送请求
+function fecthDataToSeletor(url){
+ return request.get(url)
+} 
+// 分离字段
+const splitField = ($) => {
+  let data = $('#abstract_tab_content tr:nth-child(2) td:nth-child(1)').text();
+  let store = {};
+  const goalReg = /(\[目的\/意义\])([^。])*/;
+  const methodReg = /(\[方法\/过程\])([^。])*/;
+  const resultReg = /(\[结果\/结论\])([^。])*/;
+  const goal = goalReg.exec(data)[0];
+  const method = methodReg.exec(data)[0];
+  const result = resultReg.exec(data)[0];
+  store[goal] = goal;
+  store[method] =  method;
+  store[result] = result;
+  return store;
+}
+// 写入文件
+const readFile = (store) => {
+    var json = JSON.stringify(store); // json格式解析，这步也是一定要有
     fs.writeFile('toptens.json', json, 'utf-8', function(err){
       if (err) throw err;
       else console.log('JSON写入成功'+'\r\n' + json)
     });
+}
+// 获取详情链接
+function getDetials(baseUrl) {
+ return fecthDataToSeletor(baseUrl)
+  .then(function (res) {
+    var $ = cheerio.load(res.text);
+    // console.log($(urlsSeletor))
+   return $('a[href*="[abstract"]').attr('href');
+  })
+ }
+
+
+// 开始获取
+getDetials(url).then(res => {
+  console.log(res);
+  res.forEach(data => {
+    compose(readFile,splitField,fecthDataToSeletor(data));
+  });
+  }
+ );
+
+
    
-})
-
-
-
-
 
 
